d31ce183fba7e641c23a9b5dfd729a7aaa1b3516
Matthew Henderson <info@matthewahenderson.com>
2021-03-18 16:49:33 -0400

    Finished calculator functionality in JS file; added temp display in HTML file and finished styling

--- app.js
+++ app.js
@@ -1,23 +1,111 @@
-// Variables for testing functions
-let operand1 = Math.ceil((Math.random()*10));
-let operand2 = Math.ceil((Math.random()*10));
+// Global variables for selecting calculator buttons
+const clearButton = document.querySelector('#clear-button');
+const backspaceButton = document.querySelector('#backspace-button');
+const decimalButton = document.querySelector('#decimal-button');
+const numberButton = document.querySelectorAll('.number-button');
+const equalButton = document.querySelector('#equal-button');
+const operatorButtons = document.querySelectorAll('.operator-button');
+const displayValue = document.querySelector('#display');
+const tempDisplay = document.querySelector('#temp-display')
+
+// Global variables for setting operands and operators
+let operand1 = '';
+let operand2 = '';
+let operandList = [];
+let mainOperator = null;
+
+// Add numbers to display value on click
+numberButton.forEach((button) => 
+    button.addEventListener("click", function() {displayValue.textContent += button.textContent}))
+
+// Equal function that calculates result
+function equal() {
+    operand2 = parseFloat(displayValue.textContent);
+    tempDisplay.textContent += operand2;
+    if (mainOperator === '÷' && operand2 === 0) {
+        alert('Cannot divide by zero! Try again.');
+        clear();
+        return;
+    }
+    operandList.push(operand2);
+    result = operate(operandList, mainOperator);
+    displayValue.textContent = result;
+    mainOperator = null;
+    operand1 = '';
+    operand2 = '';
+    operandList = [];
+}
+equalButton.addEventListener("click", equal)
+
+// Function for clearing display values and corresponding event listener
+function clear() {
+    operand1 = '';
+    operand2 = '';
+    operandList = '';
+    displayValue.textContent = '';
+    tempDisplay.textContent = '';
+    mainOperator = '';
+}
+clearButton.addEventListener("click", clear)
+
+// Function for backspace key for clearing previous key entry
+function backspace() {
+    displayValue.textContent = displayValue.textContent.slice(0, -1);
+}
+backspaceButton.addEventListener("click", backspace)
+
+// For each loop and function to set the operator for each operator button
+operatorButtons.forEach((button) =>
+    button.addEventListener("click", function() {declareOperator(button.textContent)}))
+
+// Function to set type of operation to be performed
+function declareOperator(operator) {
+    if (tempDisplay.textContent !== null) {
+        tempDisplay.textContent = '';
+    }
+    displayValue.textContent += operator;
+    tempDisplay.textContent += displayValue.textContent;
+    operand1 = parseFloat(displayValue.textContent);
+    operandList.push(operand1);
+    if (mainOperator === null) {
+        mainOperator = operator;
+    }
+    displayValue.textContent = '';
+}
+
+// Function to handle decimals and corresponding event listener
+function decimal() {
+    displayValue.textContent += decimalButton.textContent;
+}
+decimalButton.addEventListener("click", decimal)
+
+// Four main operations that can be called
+var operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "÷": divide
+}
+
+// Main operation function 
+function operate(list, operator) {
+    result = list.reduce(operations[operator]);
+    return result;
+}
 
 // Addition function
 function add(operand1, operand2) {
     return operand1 + operand2; 
-    // console.log(result);
 }
 
 // Subtraction function
 function subtract(operand1, operand2) {
     return operand1 - operand2;
-    // console.log(result);
 }
 
 // Multiply function
 function multiply(operand1, operand2) {
     return operand1 * operand2; 
-    // console.log(result);
 }
 
 // Divide function
@@ -25,19 +113,15 @@ function divide(operand1, operand2) {
     return operand1 / operand2;
 }
 
-// Four main operations that can be called
-var operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide
-}
-
-// Performs basic operations
-function operators(list, operator) {
-    result = list.reduce(operations[operator]);
-    return result;
-}
-
-console.log(operand1);
-console.log(operators([2, 3, 4],'/'));
+// Keyboard support 
+document.addEventListener("keypress", function (e) {
+    if (e.key >= '0' && e.key <= '9') {
+        displayValue.textContent += e.key;
+    } else if (e.key === '=') {
+        equal();
+    } else if (e.key === '.') {
+        displayValue.textContent += e.key;
+    } else if (e.key === '+' || e.key === '-' || e.key === '/' || e.key === '*') {
+        declareOperator(e.key)
+    }
+})
